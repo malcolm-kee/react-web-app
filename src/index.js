41f957764081b393fd56c41a415f8597b1a0e062
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-08-14 03:06:28 +0800

    add routes

--- src/index.js
+++ src/index.js
@@ -1,32 +1,41 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import { AppShell } from "./app-shell";
 import { AuthProvider } from "./domains/auth";
 import "./index.css";
-// import { ApplyJob } from "./pages/apply-job";
-// import { JobDetailsPage } from "./pages/job-details";
-// import { LoginPage } from "./pages/login";
+import { ApplyJob } from "./pages/apply-job";
+import { JobDetailsPage } from "./pages/job-details";
+import { LoginPage } from "./pages/login";
 
 // import { ListingDetailsPage } from "./pages/listing-details";
 // import { MarketplacePublic } from "./pages/marketplace-public";
 // import { ShoppingCart } from "./pages/shopping-cart";
 
 ReactDOM.render(
-  <AuthProvider>
-    <AppShell>
-      {/* should be shown at /login */}
-      {/* <LoginPage /> */}
-      {/* should be shown at /career */}
-      {/* <ApplyJob /> */}
-      {/* should be shown at /career/{jobId} */}
-      {/* <JobDetailsPage /> */}
-      {/* should be shown at /marketplace */}
-      {/* <MarketplacePublic /> */}
-      {/* should be shown at /listing/{listingId} */}
-      {/* <ListingDetailsPage /> */}
-      {/* should be in /shopping-cart */}
-      {/* <ShoppingCart /> */}
-    </AppShell>
-  </AuthProvider>,
+  <BrowserRouter>
+    <AuthProvider>
+      <AppShell>
+        {/* should be shown at /login */}
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        {/* should be shown at /career */}
+        <Route path="/career">
+          <ApplyJob />
+        </Route>
+        {/* should be shown at /career/{jobId} */}
+        <Route path="/career/:jobId">
+          <JobDetailsPage />
+        </Route>
+        {/* should be shown at /marketplace */}
+        {/* <MarketplacePublic /> */}
+        {/* should be shown at /listing/{listingId} */}
+        {/* <ListingDetailsPage /> */}
+        {/* should be in /shopping-cart */}
+        {/* <ShoppingCart /> */}
+      </AppShell>
+    </AuthProvider>
+  </BrowserRouter>,
   document.querySelector("#root")
 );
