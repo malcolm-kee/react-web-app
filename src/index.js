680bcf02970da08f147fef4e8681a0b9016e5e56
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-08-13 20:35:27 +0800

    comment out some components

--- src/index.js
+++ src/index.js
@@ -5,26 +5,26 @@ import { AuthProvider } from "./domains/auth";
 import "./index.css";
 import { ApplyJob } from "./pages/apply-job";
 import { JobDetailsPage } from "./pages/job-details";
-import { ListingDetailsPage } from "./pages/listing-details";
 import { LoginPage } from "./pages/login";
-import { MarketplacePublic } from "./pages/marketplace-public";
-import { ShoppingCart } from "./pages/shopping-cart";
+// import { ListingDetailsPage } from "./pages/listing-details";
+// import { MarketplacePublic } from "./pages/marketplace-public";
+// import { ShoppingCart } from "./pages/shopping-cart";
 
 ReactDOM.render(
   <AuthProvider>
     <AppShell>
       {/* should be shown at /login */}
       <LoginPage />
-      {/* should be shown at /marketplace */}
-      <MarketplacePublic />
-      {/* should be shown at /listing/{listingId} */}
-      <ListingDetailsPage />
-      {/* should be in /shopping-cart */}
-      <ShoppingCart />
       {/* should be shown at /career */}
       <ApplyJob />
       {/* should be shown at /career/{jobId} */}
       <JobDetailsPage />
+      {/* should be shown at /marketplace */}
+      {/* <MarketplacePublic /> */}
+      {/* should be shown at /listing/{listingId} */}
+      {/* <ListingDetailsPage /> */}
+      {/* should be in /shopping-cart */}
+      {/* <ShoppingCart /> */}
     </AppShell>
   </AuthProvider>,
   document.querySelector("#root")
