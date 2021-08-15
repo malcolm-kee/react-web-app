b10f442b7e297161aae28b5a7c3b5a47a851a9c8
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-08-15 22:01:47 +0800

    fix useQuery for listings

--- src/domains/marketplace/hooks/use-listings.js
+++ src/domains/marketplace/hooks/use-listings.js
@@ -5,7 +5,7 @@ import { getListingDetails, getListings } from "../marketplace.service";
 export const useListings = () => {
   const [page, setPage] = React.useState(1);
 
-  const query = useQuery("listings", () => getListings(page));
+  const query = useQuery(["listings", page], () => getListings(page));
 
   return {
     ...query,
@@ -15,18 +15,7 @@ export const useListings = () => {
 };
 
 export const useListingDetails = (listingId) => {
-  const [data, setData] = React.useState(undefined);
-
-  React.useEffect(() => {
-    const ab = new AbortController();
-    getListingDetails(listingId, ab.signal).then(setData);
-
-    return () => {
-      ab.abort();
-    };
-  }, [listingId]);
-
-  return {
-    data,
-  };
+  return useQuery(["listingDetails", listingId], () =>
+    getListingDetails(listingId)
+  );
 };
