8f13959ba7d9263d3ae110aa3282c8fc1a297113
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-08-15 22:17:18 +0800

    attach cancel property to enable cancellation

--- src/domains/career/hooks/use-jobs.js
+++ src/domains/career/hooks/use-jobs.js
@@ -5,9 +5,21 @@ import { getJobs, getJobDetails } from "../career.service";
 export const useJobs = () => {
   const [page, setPage] = React.useState(1);
 
-  const query = useQuery(["jobs", page], () => getJobs(page), {
-    staleTime: 5000,
-  });
+  const query = useQuery(
+    ["jobs", page],
+    () => {
+      const abortController = new AbortController();
+
+      const request = getJobs(page, abortController.signal);
+
+      request.cancel = () => abortController.abort();
+
+      return request;
+    },
+    {
+      staleTime: 5000,
+    }
+  );
 
   return {
     ...query,
