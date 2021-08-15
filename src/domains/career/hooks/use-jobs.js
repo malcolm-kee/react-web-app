fc78152ec3525020f93003d014eb3690e9a7ffff
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-08-15 11:32:19 +0800

    useQuery for data in career page

--- src/domains/career/hooks/use-jobs.js
+++ src/domains/career/hooks/use-jobs.js
@@ -1,26 +1,16 @@
 import * as React from "react";
+import { useQuery } from "react-query";
 import { getJobs, getJobDetails } from "../career.service";
 
 export const useJobs = () => {
-  const loadJobs = (pageNum, signal) =>
-    getJobs(pageNum, signal).then((data) => setJobs(data));
-
   const [page, setPage] = React.useState(1);
-  const [jobs, setJobs] = React.useState(undefined);
 
-  React.useEffect(() => {
-    const ab = new AbortController();
-    loadJobs(page, ab.signal);
-    return () => {
-      ab.abort();
-    };
-  }, [page]);
+  const query = useQuery("jobs", () => getJobs(page));
 
   return {
-    jobs,
+    ...query,
     page,
     setPage,
-    loadJobs,
   };
 };
 
