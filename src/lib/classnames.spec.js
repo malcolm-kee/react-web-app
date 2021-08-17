ac15cccb47c04236407751c319046bb2b530c5fc
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-08-17 23:08:44 +0800

    add first test for classnames

--- src/lib/classnames.spec.js
+++ src/lib/classnames.spec.js
@@ -1,3 +1,11 @@
+import { classNames } from "./classnames";
+
 test("canary test to make sure test setup works", () => {
   expect(true).toBe(true);
+});
+
+test("classnames combine multiple classNames into one", () => {
+  const result = classNames("text-lg", "bg-black font-bold");
+
+  expect(result).toBe("text-lg bg-black font-bold");
 });
