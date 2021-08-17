f6b740de07cba0928488340990370517ceb868e9
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-08-18 01:51:41 +0800

    install prettier and use snapshot test

--- src/components/button.spec.jsx
+++ src/components/button.spec.jsx
@@ -6,9 +6,7 @@ test('<Button variant="primary" >', () => {
 
   expect(container.firstChild.type).toBe("button");
 
-  //   console.log(container.firstChild.className);
-
-  expect(container.firstChild.className).toBe(
-    "inline-flex justify-center items-center py-2 px-4 border shadow-sm text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
+  expect(container.firstChild.className).toMatchInlineSnapshot(
+    `"inline-flex justify-center items-center py-2 px-4 border shadow-sm text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"`
   );
 });
