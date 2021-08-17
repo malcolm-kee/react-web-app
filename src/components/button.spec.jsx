988ef4e47a5cd52081110e15b11fec2876ee7b5e
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-08-18 01:52:14 +0800

    snapshot the entire button

--- src/components/button.spec.jsx
+++ src/components/button.spec.jsx
@@ -4,9 +4,12 @@ import { Button } from "./button";
 test('<Button variant="primary" >', () => {
   const { container } = render(<Button>Text</Button>);
 
-  expect(container.firstChild.type).toBe("button");
-
-  expect(container.firstChild.className).toMatchInlineSnapshot(
-    `"inline-flex justify-center items-center py-2 px-4 border shadow-sm text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"`
-  );
+  expect(container.firstChild).toMatchInlineSnapshot(`
+    <button
+      class="inline-flex justify-center items-center py-2 px-4 border shadow-sm text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
+      type="button"
+    >
+      Text
+    </button>
+  `);
 });
