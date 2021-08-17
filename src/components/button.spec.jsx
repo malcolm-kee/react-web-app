6ab9624cefdc6afb296671b9d6a81d92f9e284fd
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-08-18 01:50:04 +0800

    assert className for button

--- src/components/button.spec.jsx
+++ src/components/button.spec.jsx
@@ -5,4 +5,10 @@ test('<Button variant="primary" >', () => {
   const { container } = render(<Button>Text</Button>);
 
   expect(container.firstChild.type).toBe("button");
+
+  //   console.log(container.firstChild.className);
+
+  expect(container.firstChild.className).toBe(
+    "inline-flex justify-center items-center py-2 px-4 border shadow-sm text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
+  );
 });
